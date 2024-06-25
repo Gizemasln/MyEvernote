f4af19f458fe9732358ae7ddd957d926c99e60d4
Gizem Aslan <gzmasln03@gmail.com>
2024-06-25 16:08:30 +0300

    Update Liked.cs

--- MyEvernoteSolution/MyEvernote.Entities/Liked.cs
+++ MyEvernoteSolution/MyEvernote.Entities/Liked.cs
@@ -8,12 +8,18 @@ using System.Threading.Tasks;
 
 namespace MyEvernote.Entities
 {
+    //Note ve user arasınca like için çoka çok ilişkisi olduğu için araya bir referans tablo eklemem gerekiyor
+    //Bir notu birden fazla kullanıcı like yapabilir 
+    //bir kullanıcı bir fazla nota like yapabilir
     [Table("Likes")]
     public class Liked
     {
         [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+         //Bağlantı için virtual taımlıyorum
+        //Notun kendisi
         public virtual Note Note { get; set; }
+        //Notu likelayan kullanıcı
         public virtual EvernoteUsers LikedUser { get; set; }
     }
 }
